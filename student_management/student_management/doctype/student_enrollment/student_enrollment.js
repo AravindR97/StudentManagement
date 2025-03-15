6580793655346a91565d058f49529370e32391b5
AravindR97 <aravindrajeshchry@gmail.com>
2025-03-15 13:01:53 +0530

    feat(Task2): Add Client-Side and Seerver-Side validations

--- student_management/student_management/doctype/student_enrollment/student_enrollment.js
+++ student_management/student_management/doctype/student_enrollment/student_enrollment.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2025, Aravind and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Student Enrollment", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Student Enrollment", {
+    before_save: function (frm) {
+        var email_format = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!email_format.test(frm.doc.email)) {
+            frappe.throw("Please enter a valid email address")
+        }
+    },
+    enrollment_date: function (frm) {
+        const today = frappe.datetime.get_today()
+        if (frm.doc.enrollment_date > today) {
+            frappe.throw("Cannot select future date")
+        }
+    }
+});
--- student_management/student_management/doctype/student_enrollment/student_enrollment.py
+++ student_management/student_management/doctype/student_enrollment/student_enrollment.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2025, Aravind and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class StudentEnrollment(Document):
-	pass
+    def validate(self):
+        self.check_missing_fields()
+        self.duplicate_enrollment()
+
+    def check_missing_fields(self):
+        if not self.student_name or not self.course or not self.email:
+            frappe.throw("Missing mandatory fields")
+
+    # Additional checks
+    def duplicate_enrollment(self):
+        is_enrolled = frappe.db.exists(
+            "Student Enrollment",
+            {"student_name": self.student_name, "course": self.course, "doctstatus": 1},
+        )
+        if is_enrolled:
+            frappe.throw(f"{self.student_name} is already enrolled in {self.course}")
